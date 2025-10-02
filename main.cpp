f82826520659ddf9f16bf4a5e3a931419cf00f94
Jonas Sales <jonassales510@gmail.com>
2025-10-02 18:31:08 -0300

    updating the main to use .json file.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cerr << "Uso: " << argv[0] << " arquivo.tonto" << std::endl;
+        std::cerr << "Uso: " << argv[0] << " teste.tonto" << std::endl;
         return 1;
     }
 
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    std::string outname = std::string(argv[1]) + ".tok";
+    std::string outname = std::string(argv[1]) + ".json";
     std::ofstream out(outname);
     if (!out) {
         std::cerr << "Não foi possível criar o arquivo de saída." << std::endl;
@@ -24,8 +24,12 @@ int main(int argc, char* argv[]) {
     std::streambuf* old_cin = std::cin.rdbuf(in.rdbuf());
     std::streambuf* old_cout = std::cout.rdbuf(out.rdbuf());
 
+    out << "[" << std::endl;
+
     yyFlexLexer lexer;
     lexer.yylex();
+
+    out << std::endl << "]" << std::endl;
 
     std::cin.rdbuf(old_cin);
     std::cout.rdbuf(old_cout);
