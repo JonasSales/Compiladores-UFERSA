cf4589f798c88cb6edd89a3f57d2492df286eabf
Jonas Sales <jonassales510@gmail.com>
2025-11-13 10:25:20 -0300

    Implementando a função que escreve o syntaxAnalisys.json.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "FlexLexer.h"
 #include "analisador_lexico/lexer_utils/lex_config.h"
 #include "analisador_lexico/util/JsonWriter.h"
+#include "analisador_sintatico/Parser.h"
 
 using std::ifstream;
 using std::ofstream;
@@ -25,7 +26,7 @@ int main(int argc, char* argv[]) {
 
     ifstream in(argv[1]);
     if (!in) {
-        cerr << "Não foi possível abrir o arquivo de entrada." << endl;
+        cerr << "Nao foi possivel abrir o arquivo de entrada." << endl;
         return 1;
     }
 
@@ -38,16 +39,29 @@ int main(int argc, char* argv[]) {
     //Nome do arquivo para a lista de Tokens
     string listOutName = "testes/lexerTokenAnalisys.json";
     string analisysOutName = "testes/lexerTokenCount.json";
+    // --- SAIDA DO NOVO ANALISADOR SINTATICO ---
+    string syntaxOutName = "testes/syntaxAnalisys.json"; // <-- 2. NOVO ARQUIVO DE SAIDA SINTATICO
 
     streambuf* old_cin = cin.rdbuf(in.rdbuf());
 
     yyFlexLexer lexer;
-    lexer.yylex();
+    lexer.yylex(); // <-- O LEXICO RODA E POPULA O MAPA 'tokens'
 
     cin.rdbuf(old_cin);
 
+    // Escreve a saída do léxico (como antes)
     writeTokenList(listOutName, tokens);
     writeTokenAnalysis(analisysOutName, tokenAnalisys);
+    cout << "Analise lexica salva em " << listOutName << " e " << analisysOutName << endl;
+
+    // --- 3. EXECUTAR O ANALISADOR SINTATICO ---
+    cout << "Iniciando analise sintatica..." << endl;
+    Parser parser(tokens); // <-- O parser e alimentado com a saida do lexico
+    parser.parse();        // <-- O parser consome os tokens
+
+    // --- 4. ESCREVER A SAIDA SINTATICA ---
+    writeSyntaxAnalysis(syntaxOutName, parser);
+    cout << "Analise sintatica (Tabela de Sintese e Erros) salva em " << syntaxOutName << endl;
 
     return 0;
 }
